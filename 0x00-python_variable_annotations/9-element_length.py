e30413dd1a8c062024292fe12e393446a1c3add8
beboo22 <mt557829@gmail.com>
2024-01-11 20:46:43 +0200

    task

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
-""" x"""
+""" xddd dddd ddddd ddd"""
 from typing import Mapping, MutableMapping, Sequence, Iterable, List, Tuple
 
 
 def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
-    """ x """
+    """ xxddd dddd ddddd ddd """
     return [(i, len(i)) for i in lst]
