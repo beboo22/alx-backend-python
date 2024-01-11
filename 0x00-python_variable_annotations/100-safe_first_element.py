1369ecc500ffcf416dc391c2710ca4cb8726973f
beboo22 <mt557829@gmail.com>
2024-01-11 20:52:24 +0200

    task

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -4,6 +4,7 @@ from typing import Mapping, MutableMapping, Sequence, Iterable, List,
 Tuple, Any, Union
 
 
+# The types of the elements of the input are not know
 def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
     """ xxddd dddd ddddd ddd """
     if lst:
