81dbc56ba6c32597c491692e9d00d3b8e7f04217
beboo22 <mt557829@gmail.com>
2024-01-11 18:51:19 +0200

    task

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+import math
+
+
+'''
+    Basic annotations for variables.
+'''
+
+
+def floor(n: float) -> int:
+    '''
+        Returns the floor of a float.
+    '''
+    return math.floor(n)
