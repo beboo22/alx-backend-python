6c303705d123788907eb250e3b606cc6e641f25b
beboo22 <mt557829@gmail.com>
2024-01-11 18:45:35 +0200

    task

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+'''
+    Basic annotations for variables.
+'''
+
+
+def concat(str1: str, str2: str) -> str:
+    '''
+        Returns sum of two floats.
+    '''
+    return str1 + str2
