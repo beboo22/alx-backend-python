0313a0bb1b6c12931849e7de6f6def72254c3899
beboo22 <mt557829@gmail.com>
2024-01-11 19:58:30 +0200

    task

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+'''Basic annotations for variables.'''
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    '''Returns the floor of a float.'''
+    return sum(input_list)
