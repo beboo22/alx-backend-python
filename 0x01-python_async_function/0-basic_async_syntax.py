34381302c35ec2d835bd0b02410172d14b6d48de
beboo22 <mt557829@gmail.com>
2024-01-15 18:43:34 +0200

    task

---
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""dsffdsfsdddsdsdsf"""
+import asyncio
+import random
+
+
+async def wait_random(max_delay=10):
+    """dsffdsfsdddsdsdsf"""
+    x = random.uniform(0, max_delay)
+    await asyncio.sleep(x)
+    return x
