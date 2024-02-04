4dc97c107aca43edf718e61d69de2b8183c110bc
beboo22 <mt557829@gmail.com>
2024-02-04 16:00:41 +0200

    task

---
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""
+TestAccessNestedMap class that inherits from unittest.TestCase
+"""
+from utils import access_nested_map
+import unittest
+from parameterized import parameterized, parameterized_class
+from unittest.mock import patch
+from typing import Mapping
+
+
+class TestAccessNestedMap(unittest.TestCase):
+    @parameterized.expand([
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {"b": 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2)
+    ])
+    def test_access_nested_map(self, nested_map, path, excepted):
+        service = access_nested_map(nested_map, path)
+        self.assertEqual(service, excepted)
+
+
+if __name__ == '__main__':
+    unittest.main()
