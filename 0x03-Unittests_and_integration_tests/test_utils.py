20d5dde548b630564a26b3f6c5b1ca82fed8f93f
beboo22 <mt557829@gmail.com>
2024-02-04 18:50:00 +0200

    task

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -7,6 +7,7 @@ import unittest
 from parameterized import parameterized, parameterized_class
 from unittest.mock import patch, Mock
 from typing import Mapping
+from utils import memoize
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -38,6 +39,22 @@ class TestGetJson(unittest.TestCase):
         serv = get_json(test_url)
         self.assertEqual(serv, test_payload)
         mock_req_json.assert_called_once_with(test_url)
+
+
+class TestMemoize(unittest.TestCase):
+    def test_memoize(self):
+        class TestClass:
+            def a_method(self):
+                return 42
+
+            @memoize
+            def a_property(self):
+                return self.a_method()
+        with patch.object(TestClass, 'a_method') as mock:
+            test_class = TestClass()
+            test_class.a_property()
+            test_class.a_property()
+            mock.assert_called_once()
 
 
 if __name__ == '__main__':
