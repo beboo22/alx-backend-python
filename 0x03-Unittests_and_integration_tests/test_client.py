259729332e8350f6b28c14427b4885f3af9b7398
beboo22 <mt557829@gmail.com>
2024-02-04 19:09:58 +0200

    task

---
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+TestAccessNestedMap class that inherits from unittest.TestCase
+"""
+from client import GithubOrgClient
+import unittest
+from parameterized import parameterized, parameterized_class
+from unittest.mock import patch, Mock
+from typing import Mapping
+from utils import memoize
+
+
+class TestGithubOrgClient(unittest.TestCase):
+    @parameterized.expand([('google'), ('abc')])
+    patch('client.get_json')
+    def test_org(self, pr, mock):
+        servise = GithubOrgClient(pr)
+        servise.org(pr)
+        mock.assert_called_once_with(get_json())
